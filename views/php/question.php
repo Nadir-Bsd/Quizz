99ec47eb07095274cfb816470e9243959241e5e8
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-13 15:51:34 +0100

    ADD: HTML CSS Question Page

--- views/php/question.php
+++ views/php/question.php
@@ -4,11 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Category</title>
-    <link rel="stylesheet" href="../styles/category-and-quiz.css">
+    <link rel="stylesheet" href="../styles/question.css">
 </head>
 <body> 
 
-   
+   <section class="quiz-section">
+        <div class="quiz-box">
+            <h2>Quiz</h2>
+            <div class="quiz-header">
+                <span>Quiz Website Tutorials</span>
+                <span class="header-score">Score 0 / 5</span>
+            </div>
+
+            <h3 class="question-text">What does HTML stand for?</h3>
+
+            <div class="option-list">
+                <div class="option">
+                    <span>A. hyper Type Multi Language</span>
+                </div>
+                <div class="option">
+                    <span>B. Hyper Text Multiple Language</span>
+                </div>
+                <div class="option">
+                    <span>C. Hyper Text Markup Language</span>
+                </div>
+                <div class="option">
+                    <span>D. Home Text Multi Language</span>
+                </div>
+            </div>
+            
+            <div class="quiz-footer">
+                <span class="question-total">1 of 5 Questions</span>
+                <button class="next-btn">Next</button>
+            </div>
+        </div>
+   </section>
 
 </body>
 </html>

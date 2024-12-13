0dc7b337c0c1977526310b2e699ca457d54be08f
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-13 12:37:47 +0100

    ADD: Category page

--- views/php/category.php
+++ views/php/category.php
@@ -4,8 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Category</title>
+    <link rel="stylesheet" href="../styles/style.css">
 </head>
 <body>
-    
+    <!--  header -->
+    <header>
+        <a href="#" class="logo">Quizine.</a>
+        <nav class="navbar">
+            <a href="#" class="active">Home</a>
+            <a href="#">Profile</a>
+        </nav>
+    </header>
 </body>
 </html>

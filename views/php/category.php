7df08f1c27a664405cd8f9134986d15b99d7b16c
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-13 12:41:10 +0100

    DONE: category page

--- views/php/category.php
+++ views/php/category.php
@@ -4,16 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Category</title>
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/category.css">
 </head>
 <body>
     <!--  header -->
-    <header>
-        <a href="#" class="logo">Quizine.</a>
+    <header class="header">
+        <a href="../../index.php" class="logo">Quizine.</a>
         <nav class="navbar">
-            <a href="#" class="active">Home</a>
+            <a href="../../index.php">Home</a>
+            <a href="#" class="active">Category</a>
             <a href="#">Profile</a>
         </nav>
     </header>
+
+    <main class="container">
+        <a href="" class="category">
+            Category 1
+        </a>
+        <a href="" class="category">
+            Category 2
+        </a>
+        <a href="" class="category">
+            Category 3
+        </a>
+        <a href="" class="category">
+            Category 4
+        </a>
+    </main>
 </body>
 </html>
